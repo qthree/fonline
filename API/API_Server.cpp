f52f4371abe9f0a780ff7055f02d6c954f8efc4a
qthree <qthree3@gmail.com>
2025-08-06 02:41:19 +0700

    feat(CORROSION): impl Global_StartServerLib

--- API/API_Server.cpp
+++ API/API_Server.cpp
@@ -14,3 +14,51 @@ uint Timer_FastTick() {
 ServerGameOptions* Server_GameOptions() {
     return &GameOpt;
 }
+
+#ifdef SERVER_LIB
+#include "Version.h" 
+#include <sys/stat.h>
+FOServer Server;
+int Global_StartServerLib( int argc, const void* argv )
+{
+    // Stuff
+    setlocale( LC_ALL, "Russian" );
+    SetCommandLine( argc, argv );
+    RestoreMainDirectory();
+    CatchExceptions( "FOnlineServer", SERVER_VERSION );
+    Timer::Init();
+    Thread::SetCurrentName( "Daemon" );
+    LogToFile( "./FOnlineServerDaemon.log" );
+
+    // Config
+    IniParser cfg;
+    cfg.LoadFile( GetConfigFileName(), PT_SERVER_ROOT );
+
+    // Logging
+    LogWithTime( cfg.GetInt( "LoggingTime", 1 ) == 0 ? false : true );
+    LogWithThread( cfg.GetInt( "LoggingThread", 1 ) == 0 ? false : true );
+    if( strstr( CommandLine, "-logdebugoutput" ) || cfg.GetInt( "LoggingDebugOutput", 0 ) != 0 )
+        LogToDebugOutput();
+
+    // Log version
+    WriteLog( "FOnline server daemon, version %04X-%02X.\n", SERVER_VERSION, FO_PROTOCOL_VERSION & 0xFF );
+    if( CommandLineArgCount > 1 )
+        WriteLog( "Command line<%s>.\n", CommandLine );
+
+    umask( 0 );
+
+    GetServerOptions();
+
+    if( Server.Init() )
+    {
+        FOQuit = false;
+        Server.MainLoop();
+        Server.Finish();
+    }
+    else
+    {
+        WriteLog( "Initialization fail!\n" );
+    }
+    return 0;
+}
+#endif // SERVER_LIB
