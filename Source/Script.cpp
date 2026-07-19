7ad1d7bc3bf750a83e7ef93e463c98e5739cf4f8
qthree <qthree3@gmail.com>
2026-07-19 18:52:13 +0700

    WIP x86_64 native call

--- Source/Script.cpp
+++ Source/Script.cpp
@@ -2116,6 +2116,7 @@ string Script::GetScriptFuncName( uint func_num )
 THREAD bool              ScriptCall = false;
 THREAD asIScriptContext* CurrentCtx = NULL;
 THREAD size_t            NativeFuncAddr = 0;
+//THREAD char*             NativeFuncDecl = "";
 THREAD size_t            NativeArgs[ 256 ] = { 0 };
 THREAD size_t            NativeRetValue[ 2 ] = { 0 }; // EAX:EDX
 THREAD size_t            CurrentArg = 0;
@@ -2284,6 +2285,7 @@ bool Script::PrepareContext( int bind_id, const char* call_func, const char* ctx
         BeginExecution();
 
         NativeFuncAddr = func_addr;
+        //NativeFuncDecl = bf.FuncDecl.c_str();
         ScriptCall = false;
     }
 
@@ -2378,7 +2380,8 @@ void Script::SetArgAddress( void* value )
     CurrentArg++;
 }
 
-// Taked from AS sources
+// Taken from AngelScript/as_callfunc_x86.cpp
+// asQWORD NOINLINE CallCDeclFunction(const asDWORD *args, int paramSize, asFUNCTION_t func)
 #ifdef FO_X86
 #if defined( FO_MSVC )
 uint64 CallCDeclFunction32( const size_t* args, size_t paramSize, size_t func )
@@ -2513,6 +2516,110 @@ endcopy:
 }
 #endif // FO_X86
 
+// Taken from Source/AngelScript/as_callfunc_x64_gcc.cpp: static asQWORD __attribute__((noinline)) X64_CallFunction(const asQWORD *args, int cnt, funcptr_t func, asQWORD &retQW2, bool returnFloat) 
+#if defined(FO_GCC) && defined(FO_X64)
+typedef asQWORD ( *funcptr_t )( void );
+static asQWORD __attribute__((noinline)) CallCDeclFunction64(const asQWORD *args, int cnt, funcptr_t func, asQWORD &retQW2, bool returnFloat) 
+{
+	// Need to flag the variable as volatile so the compiler doesn't optimize out the variable
+	volatile asQWORD retQW1 = 0;
+
+	// Reference: http://www.x86-64.org/documentation/abi.pdf
+
+	__asm__ __volatile__ (
+
+		"  movq %0, %%rcx \n"	// rcx = cnt
+		"  movq %1, %%r10 \n"	// r10 = args
+		"  movq %2, %%r11 \n"	// r11 = func
+
+	// Backup stack pointer in R15 that is guaranteed to maintain its value over function calls
+		"  movq %%rsp, %%r15 \n"
+//#ifdef __OPTIMIZE__
+	// Make sure the stack unwind logic knows we've backed up the stack pointer in register r15
+	// This should only be done if any optimization is done. If no optimization (-O0) is used,
+	// then the compiler already backups the rsp before entering the inline assembler code
+		" .cfi_def_cfa_register r15 \n"
+//#endif
+
+	// Skip the first 128 bytes on the stack frame, called "red zone",  
+	// that might be used by the compiler to store temporary values
+		"  sub $128, %%rsp \n"
+
+	// Make sure the stack pointer will be aligned to 16 bytes when the function is called
+		"  movq %%rcx, %%rdx \n"
+		"  salq $3, %%rdx \n"
+		"  movq %%rsp, %%rax \n"
+		"  sub %%rdx, %%rax \n"
+		"  and $15, %%rax \n"
+		"  sub %%rax, %%rsp \n"
+
+	// Push the stack parameters, i.e. the arguments that won't be loaded into registers
+		"  movq %%rcx, %%rsi \n"
+		"  testl %%esi, %%esi \n"
+		"  jle endstack \n"
+		"  subl $1, %%esi \n"
+		"  xorl %%edx, %%edx \n"
+		"  leaq 8(, %%rsi, 8), %%rcx \n"
+		"loopstack: \n"
+		"  movq 112(%%r10, %%rdx), %%rax \n"
+		"  pushq %%rax \n"
+		"  addq $8, %%rdx \n"
+		"  cmpq %%rcx, %%rdx \n"
+		"  jne loopstack \n"
+		"endstack: \n"
+
+	// Populate integer and floating point parameters
+		"  movq %%r10, %%rax \n"
+		"  mov     (%%rax), %%rdi \n"
+		"  mov    8(%%rax), %%rsi \n"
+		"  mov   16(%%rax), %%rdx \n"
+		"  mov   24(%%rax), %%rcx \n"
+		"  mov   32(%%rax), %%r8 \n"
+		"  mov   40(%%rax), %%r9 \n"
+		"  add   $48, %%rax \n"
+		"  movsd   (%%rax), %%xmm0 \n"
+		"  movsd  8(%%rax), %%xmm1 \n"
+		"  movsd 16(%%rax), %%xmm2 \n"
+		"  movsd 24(%%rax), %%xmm3 \n"
+		"  movsd 32(%%rax), %%xmm4 \n"
+		"  movsd 40(%%rax), %%xmm5 \n"
+		"  movsd 48(%%rax), %%xmm6 \n"
+		"  movsd 56(%%rax), %%xmm7 \n"
+
+	// Call the function
+		"  call *%%r11 \n"
+
+	// Restore stack pointer
+		"  mov %%r15, %%rsp \n"
+//#ifdef __OPTIMIZE__
+	// Inform the stack unwind logic that the stack pointer has been restored
+	// This should only be done if any optimization is done. If no optimization (-O0) is used,
+	// then the compiler already backups the rsp before entering the inline assembler code
+		" .cfi_def_cfa_register rsp \n"
+//#endif
+
+	// Put return value in retQW1 and retQW2, using either RAX:RDX or XMM0:XMM1 depending on type of return value
+		"  movl %5, %%ecx \n"
+		"  testb %%cl, %%cl \n"
+		"  je intret \n"
+		"  lea %3, %%rax \n"
+		"  movq %%xmm0, (%%rax) \n"
+		"  lea %4, %%rdx \n"
+		"  movq %%xmm1, (%%rdx) \n"
+		"  jmp endcall \n"
+		"intret: \n"
+		"  movq %%rax, %3 \n"
+		"  movq %%rdx, %4 \n"
+		"endcall: \n"
+
+		: : "r" ((asQWORD)cnt), "r" (args), "r" (func), "m" (retQW1), "m" (retQW2), "m" (returnFloat)
+		: "%xmm0", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm6", "%xmm7", 
+		  "%rdi", "%rsi", "%rax", "%rdx", "%rcx", "%r8", "%r9", "%r10", "%r11", "%r15");
+
+	return retQW1;
+}
+#endif // defined(FO_GCC) && defined(FO_X64)
+
 bool Script::RunPrepared()
 {
     if( ScriptCall )
@@ -2571,8 +2678,12 @@ bool Script::RunPrepared()
     else
     {
         #ifdef FO_X86
-        *( (uint64*) NativeRetValue ) = CallCDeclFunction32( NativeArgs, CurrentArg * 4, NativeFuncAddr );
+        *( (uint64*) NativeRetValue ) = CallCDeclFunction32( NativeArgs, CurrentArg * sizeof(size_t), NativeFuncAddr );
+        #elif defined(FO_GCC) && defined(FO_X64)
+        NativeRetValue[0] = CallCDeclFunction64(NativeArgs, CurrentArg * sizeof(size_t), NativeFuncAddr, NativeRetValue[1], false);
+        //WriteLog( "CallCDeclFunction64, retQW: <%d>, retQW2: <%d>\n", NativeRetValue[0], NativeRetValue[1]);
         #endif
+
         ScriptCall = false;
     }
 
